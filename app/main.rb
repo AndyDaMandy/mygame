77c3c9fdee317a7fc8308b9cd2f7170df4c6322e
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-04-29 19:28:58 -0400

    Beginning game development

--- app/main.rb
+++ app/main.rb
@@ -1,18 +1,47 @@
-def tick args
-  args.outputs.labels  << [640, 540, 'Hello World!', 5, 1]
-  args.outputs.labels  << [640, 500, 'Docs located at ./docs/docs.html and 100+ samples located under ./samples', 5, 1]
-  args.outputs.labels  << [640, 460, 'Join the Discord server! https://discord.dragonruby.org', 5, 1]
+def handle_player_movement(args)
+  if args.inputs.left
+    args.state.player.x -= args.state.player.speed
+  elsif args.inputs.right
+    args.state.player.x += args.state.player.speed
+  end
+  if args.state.player.x +  args.state.player.w > args.grid.w
+    args.state.player.x = args.grid.w - args.state.player.w
+  end
 
-  args.outputs.sprites << { x: 576,
-                            y: 280,
-                            w: 128,
-                            h: 101,
-                            path: 'dragonruby.png',
-                            angle: args.state.tick_count }
-
-  args.outputs.labels  << { x: 640,
-                            y: 60,
-                            text: './mygame/app/main.rb',
-                            size_enum: 5,
-                            alignment_enum: 1 }
+  if args.state.player.x < 0
+    args.state.player.x = 0
+  end
 end
+
+#refactor to use spacebar here
+def jump?(args)
+  args.inputs.keyboard.key_down.z ||
+  args.inputs.keyboard.key_down.j ||
+  args.inputs.controller_one.key_down.a
+end
+def drop_down(args)
+  args.state.player.y -= args.state.player.speed / 2
+end
+
+def tick args
+  args.state.player ||= {
+    x: 100,
+    y: 100,
+    h: 64,
+    w: 64,
+    path: "sprites/main-char-2.png",
+    speed: 50
+  }
+
+  if jump?(args)
+    args.state.player.y += args.state.player.speed
+    #args.state.player.y -= args.state.player.speed / 2
+  end
+  handle_player_movement(args)
+
+  args.outputs.sprites << [args.state.player]
+  args.outputs.labels << [120, 120, "Hello World!"]
+
+
+end
+$gtk.reset
